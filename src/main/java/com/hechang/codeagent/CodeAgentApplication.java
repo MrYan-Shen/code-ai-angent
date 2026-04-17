ca37a852119d461b8bebf9b1afdd42e23a91448d
hechang <2855772356@qq.com>
2026-04-17 14:22:25 +0800

    前后端初始化，后端还包括响应包装类，自定义异常，跨域配置

---
+++ src/main/java/com/hechang/codeagent/CodeAgentApplication.java
@@ -0,0 +1,13 @@
+package com.hechang.codeagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hechang/codeagent/CodeAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hechang.codeagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
